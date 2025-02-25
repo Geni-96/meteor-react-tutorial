4bf8ef2f83a3d747d7a01214dad176e7c631d09f
Gnaneswari <gnlolugu@iu.edu>
2025-02-25 12:18:33 -0500

    filter tasks

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from 'react';
 import { useTracker, useSubscribe } from "meteor/react-meteor-data";
 import { TasksCollection } from "/imports/api/TasksCollection";
 import { Task } from "./Task";
@@ -6,10 +6,23 @@ import { TaskForm } from "./TaskForm";
 
 export const App = () => {
   const isLoading = useSubscribe("tasks");
-  const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch());
+  const [hideCompleted, setHideCompleted] = useState(false);
   const handleToggleChecked = ({ _id, isChecked }) => Meteor.callAsync("tasks.toggleChecked", { _id, isChecked });
   const handleDelete = ({ _id }) => Meteor.callAsync("tasks.delete", { _id });
+  const hideCompletedFilter = { isChecked: { $ne: true } };
+  const pendingTasksCount = useTracker(() =>
+    TasksCollection.find(hideCompletedFilter).count()
+  );
 
+  const pendingTasksTitle = `${
+    pendingTasksCount ? ` (${pendingTasksCount})` : ''
+  }`;
+
+  const tasks = useTracker(() =>
+    TasksCollection.find(hideCompleted ? hideCompletedFilter : {}, {
+      sort: { createdAt: -1 },
+    }).fetch()
+  );
   if (isLoading()) {
     return <div>Loading...</div>;
   }
@@ -18,13 +31,20 @@ export const App = () => {
       <header>
         <div className="app-bar">
           <div className="app-header">
-            <h1>Welcome to Meteor!</h1>
+          <h1>
+            📝️ To Do List
+            {pendingTasksTitle}
+          </h1>
           </div>
         </div>
       </header>
       <div className="main">
         <TaskForm />
-
+        <div className="filter">
+         <button onClick={() => setHideCompleted(!hideCompleted)}>
+           {hideCompleted ? 'Show All' : 'Hide Completed'}
+         </button>
+       </div>
         <ul className="tasks">
           {tasks.map((task) => (
             <Task
